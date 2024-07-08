33b1b162d8c2d031178bcb607a1fbfae8dcae324
Kato <zzeroyzz@gmail.com>
2024-07-08 18:38:13 -0400

    created user schema

---
+++ src/model/User.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
